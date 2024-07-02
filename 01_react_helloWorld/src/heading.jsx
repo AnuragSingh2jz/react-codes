7e25ba69d2dac0ee7a4cc8d51edd01364d955729
Anurag singh <mibanurag9140@gmail.com>
2024-07-02 10:15:24 +0530

    new file added

---
+++ 01_react_helloWorld/src/heading.jsx
@@ -0,0 +1,9 @@
+function Heading(){
+    return(
+        <>
+            <h1>Anurag singh </h1>
+            <p>soon i will be the greatest and effective coder of india</p>
+        </>
+    )
+}
+export default Heading
--- 01_react_helloWorld/src/main.jsx
+++ 01_react_helloWorld/src/main.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Heading from './heading.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Heading/>
+  </React.StrictMode>,
+)
+ReactDOM.createRoot(document.getElementById('box')).render(
+  <React.StrictMode>
+    <App/>
   </React.StrictMode>,
 )
